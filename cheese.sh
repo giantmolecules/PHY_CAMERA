488c8ec567920bb1420e7e0a5d9d392b649f724f
giantmolecules <giantmolecules@gmail.com>
2021-03-16 09:47:00 -0500

    init

--- cheese.sh
+++ cheese.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 /usr/bin/python /home/pi/PHY_CAMERA/bme280bb.py
 sudo /usr/bin/python3 /home/pi/PHY_CAMERA/turn-light-on.py
-/usr/bin/raspistill  --quality 100 --ISO 100 --exposure night --metering spot --sharpness 20 --ev 0 --drc low --contrast 20 --saturation 20 --annotate 12 -o /media/pi/SSD/still$(date +"%Y%m%dT%H%M").jpg
+/usr/bin/raspistill  --quality 100 --ISO 100 --exposure night --metering spot --sharpness 20 --ev 0 --drc low --contrast 20 --saturation 20 --annotate 12 -o /media/pi/SAMSUNG_T5/streaming16/still$(date +"%Y%m%dT%H%M").jpg
 sudo /usr/bin/raspistill --quality 75 --nopreview --ISO 100 --metering spot --ev 0 --drc low --contrast 20 --saturation 20 --annotate 12 -o /home/pi/www/still.jpg
 sudo /usr/bin/python3 /home/pi/PHY_CAMERA/turn-light-off.py
 
